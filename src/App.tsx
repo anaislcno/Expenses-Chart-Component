e253a3473c56511331638e5530f186da0814683f
An <anaislcno@gmail.com>
2023-11-09 16:36:57 +0100

    create balance component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Attribution from "./components/Attribution/Attribution";
+import Balance from "./components/Balance/Balance";
 
 function App() {
   return (
     <>
       <div className="container">
+        <Balance />
         <Attribution />
       </div>
     </>
---
+++ src/components/Balance/Balance.tsx
@@ -0,0 +1,12 @@
+import "./Balance.css";
+
+const Balance = () => {
+  return (
+    <div className="balance">
+      <h2 className="balance__title">My Balance</h2>
+      <p className="balance__p">$921.48</p>
+    </div>
+  );
+};
+
+export default Balance;
