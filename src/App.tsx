d7e147efecee626d23db1d0d80fc1519528e83b4
An <anaislcno@gmail.com>
2023-11-09 16:25:55 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Attribution from "./components/Attribution/Attribution";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Attribution />
+      </div>
+    </>
+  );
+}
+
+export default App;
